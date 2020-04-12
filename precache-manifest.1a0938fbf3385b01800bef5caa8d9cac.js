d49517d10b468f84d1127a8d91245f71a51af97c
kushal naik <naikkush11@gmail.com>
2020-04-12 18:41:03 +0530

    Updates

---
+++ precache-manifest.1a0938fbf3385b01800bef5caa8d9cac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d3feefcf1d38cf5b83d103a2d86213",
+    "url": "/kushalnaik11/robofriends/index.html"
+  },
+  {
+    "revision": "2c78ce77371bc7644def",
+    "url": "/kushalnaik11/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "9ec29f6ccbf7921bc2e2",
+    "url": "/kushalnaik11/robofriends/static/css/main.eb394cf1.chunk.css"
+  },
+  {
+    "revision": "2c78ce77371bc7644def",
+    "url": "/kushalnaik11/robofriends/static/js/2.8fde3f13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kushalnaik11/robofriends/static/js/2.8fde3f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec29f6ccbf7921bc2e2",
+    "url": "/kushalnaik11/robofriends/static/js/main.5a7513eb.chunk.js"
+  },
+  {
+    "revision": "3707e1d8013bad965ca9",
+    "url": "/kushalnaik11/robofriends/static/js/runtime-main.94f41109.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/kushalnaik11/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
